199010d68435cfdc01c8a686917d331c3e1226ed
nao-pon <hypweb@gmail.com>
2014-12-29 21:23:28 +0900

    elFinder-2.x_n-de1b5a4

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.x_n (Nightly: 86f272e) (2014-12-28)
+ * Version 2.x_n (Nightly: de1b5a4) (2014-12-29)
  * http://elfinder.org
  * 
  * Copyright 2009-2014, Studio 42
@@ -3319,7 +3319,7 @@ elFinder.prototype = {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.x_n (Nightly: 86f272e)';
+elFinder.prototype.version = '2.x_n (Nightly: de1b5a4)';
 
 
 
