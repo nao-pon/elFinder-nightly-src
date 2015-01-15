eeb64afe03ffba39f25ab81e04a893236457d718
nao-pon <hypweb@gmail.com>
2015-01-15 21:46:05 +0900

    elFinder-2.x-67e0719

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.x (Nightly: d9a806a) (2015-01-15)
+ * Version 2.x (Nightly: 67e0719) (2015-01-15)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
@@ -3030,7 +3030,7 @@ elFinder.prototype = {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.x (Nightly: d9a806a)';
+elFinder.prototype.version = '2.x (Nightly: 67e0719)';
 
 
 
@@ -8486,7 +8486,7 @@ elFinder.prototype.commands.help = function() {
 			
 			html.push(sep);
 			html.push('<div class="'+lic+'">Licence: BSD Licence</div>');
-			html.push('<div class="'+lic+'">Copyright © 2009-2014, Studio 42</div>');
+			html.push('<div class="'+lic+'">Copyright © 2009-2015, Studio 42</div>');
 			html.push('<div class="'+lic+'">„ …'+fm.i18n('dontforget')+' ”</div>');
 			html.push('</div>');
 		},
