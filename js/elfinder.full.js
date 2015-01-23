b1aeaf8f597f8c07babbf20df5a54c9b42f83374
nao-pon <hypweb@gmail.com>
2015-01-23 21:32:34 +0900

    elFinder-2.1-0ca256f

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1 (Nightly: 43c9757) (2015-01-15)
+ * Version 2.1 (Nightly: 0ca256f) (2015-01-23)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
@@ -3579,7 +3579,7 @@ elFinder.prototype = {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1 (Nightly: 43c9757)';
+elFinder.prototype.version = '2.1 (Nightly: 0ca256f)';
 
 
 
