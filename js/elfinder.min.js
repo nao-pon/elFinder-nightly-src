77fd9dfd968e2dbf07c8c0a7e358b91eea64cef6
nao-pon <hypweb@gmail.com>
2015-01-31 18:46:42 +0900

    elFinder-2.x_n-05fce7e

--- js/elfinder.min.js
+++ js/elfinder.min.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.x_n (Nightly: 05fce7e) (2015-01-30)
+ * Version 2.x_n (Nightly: 05fce7e) (2015-01-31)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
