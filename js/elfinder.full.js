515d16477b89108032bd5d894dc9ddd2c78e7eb3
nao-pon <hypweb@gmail.com>
2015-03-06 21:13:12 +0900

    elFinder-2.1-acd5f21

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1 (Nightly: 7272d0f) (2015-03-06)
+ * Version 2.1 (Nightly: acd5f21) (2015-03-06)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
@@ -3587,7 +3587,7 @@ elFinder.prototype = {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1 (Nightly: 7272d0f)';
+elFinder.prototype.version = '2.1 (Nightly: acd5f21)';
 
 
 
