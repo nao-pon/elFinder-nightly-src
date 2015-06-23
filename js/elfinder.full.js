d709dd890b2becae044003c58ef5a366e20a5c3f
nao-pon <hypweb@gmail.com>
2015-06-24 00:24:18 +0900

    elFinder-2.1-ca9446b

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1 (Nightly: d70deb6) (2015-06-23)
+ * Version 2.1 (Nightly: ca9446b) (2015-06-24)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
@@ -3757,7 +3757,7 @@ elFinder.prototype = {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1 (Nightly: d70deb6)';
+elFinder.prototype.version = '2.1 (Nightly: ca9446b)';
 
 
 
@@ -8414,11 +8414,11 @@ $.fn.elfindertree = function(fm, opts) {
 				if (opts.syncTree) {
 					if (current.length) {
 						if (!noCwd) {
-							current.parentsUntil('.'+root).filter('.'+subtree).show().addClass(expanded, function(){
+							!current.parentsUntil('.'+root).filter('.'+subtree).show().addClass(expanded, function(){
 								if (current.parent().parent().get(0) == this) {
 									scroll();
 								}
-							});
+							}).length && scroll();
 						}
 						return;
 					}
