73ed8b7993f39e2fc69e1e3ca40bed7109277b1b
nao-pon <hypweb@gmail.com>
2015-06-28 15:25:12 +0900

    elFinder-2.1_n-c3a2a5f

--- php/elFinder.class.php
+++ php/elFinder.class.php
@@ -318,9 +318,11 @@ class elFinder {
 							$this->default = $this->volumes[$id]; 
 						}
 					} else {
+						$this->removeNetVolume($volume);
 						$this->mountErrors[] = 'Driver "'.$class.'" : '.implode(' ', $volume->error());
 					}
 				} catch (Exception $e) {
+					$this->removeNetVolume($volume);
 					$this->mountErrors[] = 'Driver "'.$class.'" : '.$e->getMessage();
 				}
 			} else {
@@ -593,6 +595,22 @@ class elFinder {
 	}
 
 	/**
+	 * Remove netmount volume
+	 * 
+	 * @param object $volume
+	 */
+	protected function removeNetVolume($volume) {
+		$netVolumes = $this->getNetVolumes();
+		if (! $key = @ $volume->netMountKey) {
+			$key = md5($protocol . '-' . join('-', $options));
+		}
+		if (isset($netVolumes[$key])) {
+			unset($netVolumes[$key]);
+			$this->saveNetVolumes($netVolumes);
+		}
+	}
+
+	/**
 	 * Get plugin instance & set to $this->plugins
 	 * 
 	 * @param  string $name   Plugin name (dirctory name)
@@ -709,13 +727,7 @@ class elFinder {
 			$rootstat['netkey'] = $key;
 			return array('added' => array($rootstat));
 		} else {
-			if (! $key = @ $volume->netMountKey) {
-				$key = md5($protocol . '-' . join('-', $options));
-			}
-			if (isset($netVolumes[$key])) {
-				unset($netVolumes[$key]);
-				$this->saveNetVolumes($netVolumes);
-			}
+			$this->removeNetVolume($volume);
 			return array('error' => $this->error(self::ERROR_NETMOUNT, $args['host'], implode(' ', $volume->error())));
 		}
 
