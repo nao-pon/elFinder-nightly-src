fde4499c0af0375bea8970883e3c373376473f28
nao-pon <hypweb@gmail.com>
2015-07-16 14:31:14 +0900

    elFinder-2.1_n-3ef5689

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1_n (Nightly: 4b555ad) (2015-07-16)
+ * Version 2.1_n (Nightly: 3ef5689) (2015-07-16)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
@@ -1960,21 +1960,31 @@ window.elFinder = function(node, opts) {
 
 	if (self.dragUpload) {
 		node[0].addEventListener('dragenter', function(e) {
-			e.preventDefault();
-			e.stopPropagation();
+			if (e.target.nodeName !== 'TEXTAREA' && e.target.nodeName !== 'INPUT') {
+				e.preventDefault();
+				e.stopPropagation();
+			}
 		}, false);
 		node[0].addEventListener('dragleave', function(e) {
-			e.preventDefault();
-			e.stopPropagation();
+			if (e.target.nodeName !== 'TEXTAREA' && e.target.nodeName !== 'INPUT') {
+				e.preventDefault();
+				e.stopPropagation();
+			}
 		}, false);
 		node[0].addEventListener('dragover', function(e) {
-			e.preventDefault();
-			e.stopPropagation();
+			if (e.target.nodeName !== 'TEXTAREA' && e.target.nodeName !== 'INPUT') {
+				e.preventDefault();
+				e.stopPropagation();
+			}
 		}, false);
 		node[0].addEventListener('drop', function(e) {
-			e.preventDefault();
-			e.stopPropagation();
-			self.error(['errUploadFile', self.i18n('items'), 'errPerm']);
+			if (e.target.nodeName !== 'TEXTAREA' && e.target.nodeName !== 'INPUT') {
+				e.stopPropagation();
+				e.preventDefault();
+				if ($(e.target).is('[class*="elfinder"]')) {
+					self.error(['errUploadFile', self.i18n('items'), 'errPerm']);
+				}
+			}
 		}, false);
 	}
 	
@@ -3940,7 +3950,7 @@ if (!Object.keys) {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1_n (Nightly: 4b555ad)';
+elFinder.prototype.version = '2.1_n (Nightly: 3ef5689)';
 
 
 
