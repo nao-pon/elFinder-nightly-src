17e272ac217e713e38686a3d140e96c5010dc3a4
nao-pon <hypweb@gmail.com>
2015-07-31 16:52:29 +0900

    elFinder-2.1-e85e0fa

--- js/elfinder.full.js
+++ js/elfinder.full.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1 (Nightly: 857219b) (2015-07-31)
+ * Version 2.1 (Nightly: e85e0fa) (2015-07-31)
  * http://elfinder.org
  * 
  * Copyright 2009-2015, Studio 42
@@ -3951,7 +3951,7 @@ if (!Object.keys) {
  *
  * @type String
  **/
-elFinder.prototype.version = '2.1 (Nightly: 857219b)';
+elFinder.prototype.version = '2.1 (Nightly: e85e0fa)';
 
 
 
@@ -7063,17 +7063,21 @@ $.fn.elfindercwd = function(fm, options) {
 				.hide(),
 			
 			restm = null,
-			resize = function() {
-				restm && clearTimeout(restm);
-				restm = setTimeout(function(){
-					var h = 0, wph, cwdoh;
-
+			resize = function(init) {
+				var initHeight = function() {
+					var h = 0;
 					wrapper.siblings('div.elfinder-panel:visible').each(function() {
 						h += $(this).outerHeight(true);
 					});
-
 					wrapper.height(wz.height() - h - wrapper._padding);
-
+				};
+				
+				init && initHeight();
+				
+				restm && clearTimeout(restm);
+				restm = setTimeout(function(){
+					!init && initHeight();
+					var wph, cwdoh;
 					// fix cwd height if it less then wrapper
 					cwd.css('height', 'auto');
 					wph = wrapper[0].clientHeight - parseInt(wrapper.css('padding-top')) - parseInt(wrapper.css('padding-bottom')),
@@ -7170,7 +7174,7 @@ $.fn.elfindercwd = function(fm, options) {
 			})
 			.bind('resize', function() {
 				var place = list ? cwd.find('tbody') : cwd;
-				resize();
+				resize(true);
 				bottomMarkerShow(place, place.find('[id]').length);
 			})
 			.bind('add', function() {
